fae4c29349488afe423039ef4597e88ab568821b
hadidabomb2 <hadi.rizvi123@gmail.com>
2020-02-07 19:50:09 +0000

    Creating tests for Parcel properties

---
+++ src/test/java/ParcelTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+public class ParcelTest {
+
+    @Test
+    public void testingSmallParcelProperties(){
+        Parcel parcel = new Parcel(1,1,1);
+        assertEquals(parcel.getCost(), 3);
+        assertEquals(parcel.getSize(), "Small");
+    }
+    @Test
+    public void testingMediumParcelProperties(){
+        Parcel parcel = new Parcel(10,10,1);
+        assertEquals(parcel.getCost(), 8);
+        assertEquals(parcel.getSize(), "Medium");
+    }
+    @Test
+    public void testingLargeParcelProperties(){
+        Parcel parcel = new Parcel(50,5,50);
+        assertEquals(parcel.getCost(), 15);
+        assertEquals(parcel.getSize(), "Large");
+    }
+    @Test
+    public void testingXLParcelProperties(){
+        Parcel parcel = new Parcel(100,100,10);
+        assertEquals(parcel.getCost(), 25);
+        assertEquals(parcel.getSize(), "XL");
+    }
+
+}
