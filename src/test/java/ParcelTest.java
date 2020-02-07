de523eeeabe96397ee9735ede79f1574dded739e
hadidabomb2 <hadi.rizvi123@gmail.com>
2020-02-07 19:59:22 +0000

    Refactoring parcel tests to change size to a type Enum

--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -8,25 +8,25 @@ public class ParcelTest {
     public void testingSmallParcelProperties(){
         Parcel parcel = new Parcel(1,1,1);
         assertEquals(parcel.getCost(), 3);
-        assertEquals(parcel.getSize(), "Small");
+        assertEquals(parcel.getSize(), Sizes.SMALL);
     }
     @Test
     public void testingMediumParcelProperties(){
         Parcel parcel = new Parcel(10,10,1);
         assertEquals(parcel.getCost(), 8);
-        assertEquals(parcel.getSize(), "Medium");
+        assertEquals(parcel.getSize(), Sizes.MEDIUM);
     }
     @Test
     public void testingLargeParcelProperties(){
         Parcel parcel = new Parcel(50,5,50);
         assertEquals(parcel.getCost(), 15);
-        assertEquals(parcel.getSize(), "Large");
+        assertEquals(parcel.getSize(), Sizes.LARGE);
     }
     @Test
     public void testingXLParcelProperties(){
         Parcel parcel = new Parcel(100,100,10);
         assertEquals(parcel.getCost(), 25);
-        assertEquals(parcel.getSize(), "XL");
+        assertEquals(parcel.getSize(), Sizes.XL);
     }
 
 }
