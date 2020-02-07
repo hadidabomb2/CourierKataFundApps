2eba56f89350ad1e4a828ea54c93a1126babcda9
hadidabomb2 <hadi.rizvi123@gmail.com>
2020-02-07 20:02:57 +0000

    Adding Enum class called Sizes and refactoring Parcel class to take this new Enum into account.

--- src/main/java/Parcel.java
+++ src/main/java/Parcel.java
@@ -1,6 +1,6 @@
 public class Parcel {
     private int cost;
-    private String size;
+    private Sizes size;
     private int height;
     private int width;
     private int length;
@@ -9,7 +9,31 @@ public class Parcel {
         height = heightIn;
         width = widthIn;
         length = lengthIn;
+        assignCostAndSize();
+    }
 
+    private void assignCostAndSize(){
+        int largestDimension = findLargestDimension();
+
+        if(largestDimension < 10){
+            cost = 3;
+            size = Sizes.SMALL;
+        }
+        else if(largestDimension < 50){
+            cost = 8;
+            size = Sizes.MEDIUM;
+        }
+        else if(largestDimension < 100){
+            cost = 15;
+            size = Sizes.LARGE;
+        }
+        else{
+            cost = 25;
+            size = Sizes.XL;
+        }
+    }
+
+    private int findLargestDimension(){
         int largestDimension = height;
         if(width > largestDimension){
             largestDimension = width;
@@ -17,25 +41,10 @@ public class Parcel {
         if(length > largestDimension){
             largestDimension = length;
         }
-        if(largestDimension < 10){
-            cost = 3;
-            size = "Small";
-        }
-        else if(largestDimension < 50){
-            cost = 8;
-            size = "Medium";
-        }
-        else if(largestDimension < 100){
-            cost = 15;
-            size = "Large";
-        }
-        else{
-            cost = 25;
-            size = "XL";
-        }
+        return largestDimension;
     }
 
-    public String getSize(){
+    public Sizes getSize(){
         return size;
     }
 
---
+++ src/main/java/Sizes.java
@@ -0,0 +1,3 @@
+public enum Sizes {
+    SMALL, MEDIUM, LARGE, XL;
+}
