d4e5c44328083460780878445072c6accbebf470
hadidabomb2 <hadi.rizvi123@gmail.com>
2020-02-07 20:29:30 +0000

    Refactoring all test classes to take parcel weight into account

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -6,21 +6,21 @@ public class OrderTest {
     @Test
     public void testingMakeParcelFunctionality(){
         Order order = new Order();
-        order.makeParcel(1,1,1);
+        order.makeParcel(1,1,1, 1);
         assertEquals(order.getTotalCost(), 3);
     }
 
     @Test
     public void testingFinishOrderFunctionality(){
         Order order = new Order();
-        order.makeParcel(1,1,1);
+        order.makeParcel(1,1,1, 1);
         String output = order.finishOrder();
         assertEquals(output, "Cost of parcel: 3. Total cost: 3.");
     }
     @Test
     public void testingMakeOrderSpeedyShippingFunctionality(){
         Order order = new Order();
-        order.makeParcel(1,1,1);
+        order.makeParcel(1,1,1, 1);
         order.toggleSpeedyShipping();
         String output = order.finishOrder();
         assertEquals(output, "Cost of parcel: 3. Cost of speedy shipping: 3. Total cost: 6.");
--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -6,27 +6,42 @@ public class ParcelTest {
 
     @Test
     public void testingSmallParcelProperties(){
-        Parcel parcel = new Parcel(1,1,1);
+        Parcel parcel = new Parcel(1,1,1, 1);
         assertEquals(parcel.getCost(), 3);
         assertEquals(parcel.getSize(), Sizes.SMALL);
     }
     @Test
     public void testingMediumParcelProperties(){
-        Parcel parcel = new Parcel(10,10,1);
+        Parcel parcel = new Parcel(10,10,1, 1);
         assertEquals(parcel.getCost(), 8);
         assertEquals(parcel.getSize(), Sizes.MEDIUM);
     }
     @Test
     public void testingLargeParcelProperties(){
-        Parcel parcel = new Parcel(50,5,50);
+        Parcel parcel = new Parcel(50,5,50, 1);
         assertEquals(parcel.getCost(), 15);
         assertEquals(parcel.getSize(), Sizes.LARGE);
     }
     @Test
     public void testingXLParcelProperties(){
-        Parcel parcel = new Parcel(100,100,10);
+        Parcel parcel = new Parcel(100,100,10, 1);
         assertEquals(parcel.getCost(), 25);
         assertEquals(parcel.getSize(), Sizes.XL);
+    }
+    @Test
+    public void testingForUnderWeightParcel(){
+        Parcel parcel = new Parcel(10,10,10,1);
+        assertEquals(parcel.getCost(), 8);
+    }
+    @Test
+    public void testingForExactWeightOnLimitParcel(){
+        Parcel parcel = new Parcel(10,10,10,3);
+        assertEquals(parcel.getCost(), 8);
+    }
+    @Test
+    public void testingForOverWeightParcel(){
+        Parcel parcel = new Parcel(10,10,10,5.5);
+        assertEquals(parcel.getCost(), 8);
     }
 
 }
