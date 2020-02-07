de7bf7a105c11b24400d4845a621cafe44fe8a5d
hadidabomb2 <hadi.rizvi123@gmail.com>
2020-02-07 20:58:05 +0000

    Adding tests for discount functionality in ordertest class

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -26,4 +26,38 @@ public class OrderTest {
         assertEquals(output, "Cost of parcel: 3. Cost of speedy shipping: 3. Total cost: 6.");
         assertEquals(order.getTotalCost(), 6);
     }
+    @Test
+    public void testingMediumDiscountFunctionality(){
+        Order order = new Order();
+        order.makeParcel(10,10,10,3);
+        order.makeParcel(10,10,10,3);
+        order.makeParcel(10,10,10,3);
+        order.makeParcel(10,10,10,4);
+        order.makeParcel(10,10,10,4);
+        order.makeParcel(10,10,10,4);
+        order.finishOrder();
+        assertEquals(order.getTotalCost(), 36);
+    }
+    @Test
+    public void testingSmallDiscountFunctionality(){
+        Order order = new Order();
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,1);
+        order.finishOrder();
+        assertEquals(order.getTotalCost(), 12);
+    }
+    @Test
+    public void testingMixedDiscountFunctionality(){
+        Order order = new Order();
+        order.makeParcel(10,10,10,3);
+        order.makeParcel(10,10,10,3);
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,1);
+        order.makeParcel(1,1,1,2);
+        order.finishOrder();
+        assertEquals(order.getTotalCost(), 24);
+    }
 }
